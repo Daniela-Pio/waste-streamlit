c8e5135880f835c0c67038dd08c97711f8285a4b
Daniela-Pio <156179309+Daniela-Pio@users.noreply.github.com>
2024-03-23 12:12:55 +0000

    Updated streamlit_app.py with changes to display data in a bar chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,47 +4,48 @@ import pandas as pd
 import altair as alt
 
 # Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
+st.set_page_config(page_title='Waste dedication', page_icon='🗑️')
+st.title('🗑️ Waste dedication')
 
 with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
-  
-st.subheader('Which Movie Genre performs ($) best at the box office?')
+st.markdown('What can this app do?')
+st.info('This app gives you information about the composition of the contents of the waste bins and the further processing of the waste.')
+st.markdown('How to use the app?')
+st.warning('Select the desired waste bin and you will receive the desired information.')
+
 
 # Load data
-df = pd.read_csv('data/movies_genres_summary.csv')
-df.year = df.year.astype('int')
+data = pd.DataFrame({
+'Mülltonne': ['Schwarze Tonne', 'Gelbe Tonne', 'Braune Tonne'],
+'Plastik': [70, 90, 0],
+'Papier': [20, 0, 10],
+'Restmüll': [10, 0, 10],
+'Wasser': [0, 10, 0],
+'Organische Abfälle': [0, 0, 80]
+})
 
 # Input widgets
-## Genres selection
-genres_list = df.genre.unique()
-genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
+## Bin selection
+waste_type = st.selectbox('Select Trash bin', data['Mülltonne'])
 
-## Year selection
-year_list = df.year.unique()
-year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
-year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
-
-df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
+selected_data = data[data['Mülltonne'] == waste_type].melt(var_name='Bestandteil', value_name='Prozent')
 
 
-# Display DataFrame
-
-df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
-                            column_config={"year": st.column_config.TextColumn("Year")},
-                            num_rows="dynamic")
-df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
 
 # Display chart
-chart = alt.Chart(df_chart).mark_line().encode(
-            x=alt.X('year:N', title='Year'),
-            y=alt.Y('gross:Q', title='Gross earnings ($)'),
-            color='genre:N'
-            ).properties(height=320)
-st.altair_chart(chart, use_container_width=True)
+chart = alt.Chart(selected_data).mark_bar().encode(
+x='Prozent:Q',
+y='Bestandteil:N'
+).properties(
+width=600,
+height=400
+)
+
+st.altair_chart(chart)
+
+#Infotext
+if waste_type == 'Schwarze Tonne':
+st.write("Der Müll aus der schwarzen Tonne wird in einer Müllverbrennungsanlage verbrannt.")
+elif waste_type == 'Gelbe Tonne':
+st.write("Der Plastikmüll aus der gelben Tonne wird recycelt und wiederverwendet.")
+elif waste_type == 'Braune Tonne':
